a4f4d1c902f31d14da98feecd01f23f9d32bbbf9
Justin Sam <justinsam02544@gmail.com>
2023-02-01 12:46:55 -0500

    Changed the name, Id and Code in the main method using the Constructor

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -9,6 +9,10 @@ public class NewClass {
     
      public static void main(String[] args) {
         System.out.println("Hello World!");
+       Course driver = new Course(123456,"Justin","CSC229");
+       System.out.println("Name: "+ driver.getName() + " ID: " + driver.getID() + " Code: " + driver.getCode());
     }
+
+   
     
 }
